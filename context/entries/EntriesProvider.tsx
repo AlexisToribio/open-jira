800f81a94285a38d844b5a51fd7a7adca812944d
AlexisToribio <alexistoba21@hotmail.com>
2023-07-12 19:14:55 -0500

    feat: add update state entries

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -25,8 +25,16 @@ export const EntriesProvider = ({ children }: Props) => {
     dispatch({ type: '[Entry] Add-Entry', payload: data });
   };
 
-  const updateEntry = (entry: Entry) => {
-    dispatch({ type: '[Entry] Entry-Updated', payload: entry });
+  const updateEntry = async ({ _id, description, status }: Entry) => {
+    try {
+      const { data } = await entriesApi.put<Entry>(`/entries/${_id}`, {
+        description,
+        status,
+      });
+      dispatch({ type: '[Entry] Entry-Updated', payload: data });
+    } catch (error) {
+      console.log({ error });
+    }
   };
 
   const refreshEntries = async () => {
