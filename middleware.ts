496df291c4616fa536ec01976d8ef3465597fb2d
AlexisToribio <alexistoba21@hotmail.com>
2023-07-12 21:06:28 -0500

    refactor: remove console.log

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,6 @@ import type { NextRequest } from 'next/server';
 export function middleware(req: NextRequest) {
   if (req.nextUrl.pathname.startsWith('/api/entries')) {
     const id = req.nextUrl.pathname.replace('/api/entries/', '');
-    console.log(id);
     const checkMongoIDRegExp = new RegExp('^[0-9a-fA-F]{24}$');
     if (!checkMongoIDRegExp.test(id)) {
       const url = req.nextUrl.clone();
